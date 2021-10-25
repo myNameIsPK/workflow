181b304f23bc7802b82a5e88919bcfe9db0806e3
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2021-10-25 21:16:27 +0700

    FIX: alias autocmd not source!

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -6,3 +6,13 @@ vim.cmd(
     augroup END
   ]]
 )
+
+-- FIXME: file not source
+-- vim.cmd(
+--   [[
+--     augroup aliases
+--       au!
+--       au BufWritePost $XDG_CONFIG_HOME/shell/aliases !source $XDG_CONFIG_HOME/shell/aliases
+--     augroup END
+--   ]]
+-- )
