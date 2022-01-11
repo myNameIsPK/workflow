1ac192f212add144b896dca53722d53f75e9bbca
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-01-12 01:19:33 +0700

    CHANGE: move config from ftplugin to unified autocmds for general settings

--- after/ftplugin/man.lua
+++
@@ -1,6 +0,0 @@
-vim.cmd [[
-  augroup ManKeymap
-    au!
-    au FileType man nnoremap <buffer><silent> q :quit<CR>"
-  augroup End
-]]
--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -1,23 +1,24 @@
 vim.cmd(
   [[
-    augroup TextYankHighlight
-      au!
-      au TextYankPost * silent! lua vim.highlight.on_yank()
+    augroup _general_settings
+      autocmd!
+      autocmd TextYankPost * silent! lua vim.highlight.on_yank()
+      autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
     augroup END
 
     augroup SpellAdd
-      au!
-      au BufWritePost *.utf-8.add :mkspell! ~/.config/nvim/spell/en.utf-8.add
+      autocmd!
+      autocmd BufWritePost *.utf-8.add :mkspell! ~/.config/nvim/spell/en.utf-8.add
     augroup END
 
     augroup Xresources
-      au!
-      au BufWritePost xresources :!xrdb -merge ~/.config/x11/xresources
+      autocmd!
+      autocmd BufWritePost xresources :!xrdb -merge ~/.config/x11/xresources
     augroup END
 
     augroup Sxhkdrc
-      au!
-      au BufWritePost sxhkdrc :!pkill -USR1 -x sxhkd; notify-send reload sxhkd
+      autocmd!
+      autocmd BufWritePost sxhkdrc :!pkill -USR1 -x sxhkd; notify-send reload sxhkd
     augroup END
   ]]
 )
@@ -26,8 +27,8 @@ vim.cmd(
 -- vim.cmd(
 --   [[
 --     augroup aliases
---       au!
---       au BufWritePost $XDG_CONFIG_HOME/shell/aliases !source $XDG_CONFIG_HOME/shell/aliases
+--       autocmd!
+--       autocmd BufWritePost $XDG_CONFIG_HOME/shell/aliases !source $XDG_CONFIG_HOME/shell/aliases
 --     augroup END
 --   ]]
 -- )
