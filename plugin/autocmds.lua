6cd558c8d8b2a45e10bb0e1fb55914f64bcf5be3
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-01-13 05:42:13 +0700

    UPDATE: autocmds

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -6,26 +6,31 @@ vim.cmd(
       autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
     augroup END
 
-    augroup SpellAdd
+    augroup spellAdd
       autocmd!
       autocmd BufWritePost *.utf-8.add :mkspell! ~/.config/nvim/spell/en.utf-8.add
     augroup END
 
-    augroup Xresources
+    augroup xresources
       autocmd!
       autocmd BufWritePost xresources :!xrdb -merge ~/.config/x11/xresources
     augroup END
 
-    augroup Sxhkdrc
+    augroup sxhkdrc
       autocmd!
-      autocmd BufWritePost sxhkdrc :!pkill -USR1 -x sxhkd; notify-send reload sxhkd
+      autocmd BufWritePost sxhkdrc :!pkill -USR1 -x sxhkd; notify-send sxhkd reload\!\!
     augroup END
+
   ]]
 )
 
 -- FIXME: file not source
 -- vim.cmd(
 --   [[
+--     augroup _polybar
+--      autocmd!
+--      autocmd BufWritePost $XDG_CONFIG_HOME/polybar/config :!source $XDG_CONFIG_HOME/polybar/launch ; notify-send polybar reloaded\!\!
+--     augroup END
 --     augroup aliases
 --       autocmd!
 --       autocmd BufWritePost $XDG_CONFIG_HOME/shell/aliases !source $XDG_CONFIG_HOME/shell/aliases
