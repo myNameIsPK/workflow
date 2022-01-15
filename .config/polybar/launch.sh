f994205c852cd6d85dd6845c9dd22b4dc8704247
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-01-16 00:04:39 +0700

    UPDATE: my personal polybar config

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -6,7 +6,7 @@ killall -q polybar
 # polybar-msg cmd quit
 
 # Launch bar1 and bar2
-echo "---" | tee -a /tmp/polybar_example.log
-polybar example 2>&1 | tee -a /tmp/polybar_example.log & disown
+echo "---" | tee -a /tmp/polybar_mybar.log
+polybar mybar 2>&1 | tee -a /tmp/polybar_mybar.log & disown
 
 echo "Bars launched..."
