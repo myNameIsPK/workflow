1eecb7e88790e6c10133aeb9d18a02c2130591d3
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-05-10 02:28:37 +0700

    UPDATE: keymap updates

--- lua/highlight.lua
+++ lua/highlight.lua
@@ -6,6 +6,7 @@ if plugin_installed("gruvbox-material") then
   vim.g.gruvbox_material_visual = "reverse"
   vim.g.gruvbox_material_background = "soft"
   vim.g.gruvbox_material_disable_italic_comment = 0
+  vim.g.gruvbox_material_palette = "original"
   vim.cmd "colorscheme gruvbox-material"
 else
   -- Colors:Gruvbox
--- plugin/keymappings.lua
+++ plugin/keymappings.lua
@@ -3,8 +3,8 @@ local plugin_installed = require("utils.plugins").plugin_installed
 
 map("i", { "jk", "kj", "jj", "kk" }, "<Esc>")
 
-map("i", "<M-j>", "<Esc>:m .+1<CR>==gi")
-map("i", "<M-k>", "<Esc>:m .-2<CR>==gi")
+map("i", "<m-j>", "<esc>:m .+1<cr>==gi")
+map("i", "<m-k>", "<esc>:m .-2<cr>==gi")
 
 map("i", ",", ",<c-g>u")
 map("i", ".", ".<c-g>u")
@@ -43,15 +43,8 @@ map("n", "<expr> k", "(v:count > 1 ? \"m'\" . v:count : '') . 'k'")
 map("v", "<", "<gv")
 map("v", ">", ">gv")
 
-map("v", "K", ":m '<-2<CR>gv=gv") -- FIXME: concurent with lsp hover
-map("v", "J", ":m '>+1<CR>gv=gv")
-map("v", "<M-k>", ":m '<-2<CR>gv=gv")
-map("v", "<M-j>", ":m '>+1<CR>gv=gv")
-
-map("x", "K", ":m '<-2<CR>gv=gv") -- FIXME: concurent with lsp hover
-map("x", "J", ":m '>+1<CR>gv=gv")
-map("x", "<M-k>", ":m '<-2<CR>gv=gv")
-map("x", "<M-j>", ":m '>+1<CR>gv=gv")
+map({ "v", "x" } , "<M-k>", ":m '<-2<CR>gv=gv")
+map({ "v", "x" } , "<M-j>", ":m '>+1<CR>gv=gv")
 
 map("v", "p", '"_dP') -- not yank before paste in visual select
 
@@ -70,6 +63,12 @@ map("c", "<C-k>", 'pumvisible() ? "\\<C-p>" : "\\<C-k>"', { expr = true, noremap
 -- disable because sandwich.vim, `s` could be replaced by `cl`
 map({ "n", "x" }, "s", "<Nop>")
 
+-- See `:help vim.diagnostic.*` for documentation on any of the below functions
+map('n', '<leader>de', function() vim.diagnostic.open_float() end, { desc = "Diagnostic Float" })
+map('n', '[d', function() vim.diagnostic.goto_prev() end, { desc = "Diagnostic Prev" })
+map('n', '<leader>dq', function() vim.diagnostic.setloclist() end, { desc = "Diagnostic Quixfix" })
+map('n', ']d', function() vim.diagnostic.goto_next() end, { desc = "Diagnostic Next" })
+
 -- Telescope
 if plugin_installed "telescope.nvim" then
   -- Builtin
@@ -80,6 +79,9 @@ if plugin_installed "telescope.nvim" then
   map("n", "<leader>fb", "<Cmd>Telescope buffers<Cr>", { desc = "Help Tags" })
   map("n", "<leader>fr", "<Cmd>Telescope oldfiles<Cr>", { desc = "Recent Files" })
 
+  map('n', '<leader>fdb', '<cmd>Telescope diagnostics bufnr=0<cr>', { desc = "Diagnostics Buffer" })
+  map('n', '<leader>fdw', '<cmd>Telescope diagnostics<cr>', { desc = "Diagnostics Workspace" })
+
   -- Loaded Extensions
   map("n", "<leader>fp", "<Cmd>Telescope projects<Cr>", { desc = "Projects" })
 
@@ -89,15 +91,6 @@ if plugin_installed "telescope.nvim" then
   map("n", "<leader>fD", require("plugins.telescope").find_dotfiles, { desc = "Dotfiles" })
   map("n", "<leader>td", require("plugins.telescope").todo_comments, { desc = "TODO comments" })
 
-  local wk_ok, wk = pcall(require, "which-key")
-  if wk_ok then
-    wk.register {
-      ["<leader>f"] = "+Telescope",
-      ["<leader>d"] = "Diagnostics", -- in diagnostic.lua
-      ["<leader>l"] = "LSP", -- in lsp_handlers.lua
-      ["<leader>h"] = "Hunk(gitsigns)", -- in gitsigns.lua
-    }
-  end
 end
 
 if plugin_installed("zk-nvim") then
@@ -107,4 +100,30 @@ end
 
 if plugin_installed("neorg") then
   map("n", "<leader>ng", "<Cmd>NeorgStart<Cr>")
+end
+
+if plugin_installed("nvim-lsp-installer") then
+  map("n", "<leader>sl", "<Cmd>LspInfo<Cr>")
+  map("n", "<leader>tl", "<Cmd>LspInstallInfo<Cr>")
+end
+
+if plugin_installed("nvim-treesitter") then
+  map("n", "<leader>st", "<Cmd>TSModuleInfo<Cr>")
+  map("n", "<leader>tp", "<Cmd>TSPlaygroundToggl<Cr>")
+end
+
+if plugin_installed("neorg") then
+  map("n", "<leader>tC", "<Cmd>ColorizerToggle<Cr>")
+end
+
+local wk_ok, wk = pcall(require, "which-key")
+if wk_ok then
+  wk.register {
+    ["<leader>f"] = "Telescope",
+    ["<leader>d"] = "Diagnostics", -- in diagnostic.lua
+    ["<leader>l"] = "LSP", -- in lsp_handlers.lua
+    ["<leader>h"] = "Hunk(gitsigns)", -- in gitsigns.lua
+    ["<leader>s"] = "Status",
+    ["<leader>t"] = "Toggle",
+  }
 end
