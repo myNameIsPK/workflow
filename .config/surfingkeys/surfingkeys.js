82be0639076b9aea004a34ae765016f6e4fa1907
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-05-10 22:17:43 +0700

    UPDATE: add surfingkeys(browser extensions) config

---
+++ .config/surfingkeys/surfingkeys.js
@@ -0,0 +1,75 @@
+// from: https://github.com/brookhong/Surfingkeys/wiki/Migrate-your-settings-from-0.9.74-to-1.0
+const {
+    aceVimMap,
+    mapkey,
+    imap,
+    imapkey,
+    getClickableElements,
+    vmapkey,
+    map,
+    unmap,
+    vunmap,
+    cmap,
+    addSearchAlias,
+    removeSearchAlias,
+    tabOpenLink,
+    readText,
+    Clipboard,
+    Front,
+    Hints,
+    Visual,
+    RUNTIME
+} = api;
+
+// FIX: seem not work
+// an example to create a new mapping `ctrl-y`
+mapkey('<ctrl-y>', 'Show me the money', function() {
+    Front.showPopup('a well-known phrase uttered by characters in the 1996 film Jerry Maguire (Escape to close).');
+});
+
+// FIX: seem not work
+// an example to remove mapkey `Ctrl-i`
+unmap('<ctrl-i>');
+
+// an example to replace `T` with `gt`, click `Default mappings` to see how `T` works.
+map('gt', 'T');
+
+removeSearchAlias('b'); //baidu
+removeSearchAlias('e'); //wikipedia
+removeSearchAlias('w'); //bing
+
+// FIX: not work
+// settings.useNeovim = true
+
+// gruvbox dark
+settings.theme = `
+.sk_theme {
+    font-family: Input Sans Condensed, Charcoal, sans-serif;
+    font-size: 10pt;
+    background: #282828;
+    color: #ebdbb2;
+}
+.sk_theme tbody {
+    color: #b8bb26;
+}
+.sk_theme input {
+    color: #d9dce0;
+}
+.sk_theme .url {
+    color: #98971a;
+}
+.sk_theme .annotation {
+    color: #b16286;
+}
+.sk_theme .omnibar_highlight {
+    color: #ebdbb2;
+}
+.sk_theme #sk_omnibarSearchResult ul li:nth-child(odd) {
+    background: #282828;
+}
+.sk_theme #sk_omnibarSearchResult ul li.focused {
+    background: #d3869b;
+}
+#sk_status, #sk_find {
+    font-size: 10pt;
+}`;
