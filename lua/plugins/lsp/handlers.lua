be7dd58af06216a8efc724e556f8967ee342ff08
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-05-11 21:44:24 +0700

    UPDATE: lsp keybinds

--- lua/plugins/lsp/handlers.lua
+++ lua/plugins/lsp/handlers.lua
@@ -1,40 +1,41 @@
 local M = {}
 
 local function lsp_keymaps(bufnr)
-  local function buf_set_keymap(...) vim.api.nvim_buf_set_keymap(bufnr, ...) end
   local function buf_set_option(...) vim.api.nvim_buf_set_option(bufnr, ...) end
 
   -- Enable completion triggered by <c-x><c-o>
   buf_set_option('omnifunc', 'v:lua.vim.lsp.omnifunc')
 
-  local opts = { noremap=true, silent=true }
+  local map = require("utils.mappings").map
+  local lsp = vim.lsp.buf
 
-  -- See `:help vim.lsp.*` for documentation on any of the below functions
-  buf_set_keymap('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', opts)
-  buf_set_keymap('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', opts)
-  buf_set_keymap('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>', opts)
-  buf_set_keymap('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>', opts)
-  buf_set_keymap('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
-  buf_set_keymap('n', '<C-k>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', opts)
-  buf_set_keymap('n', '<leader>lwa', '<cmd>lua vim.lsp.buf.add_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>lwr', '<cmd>lua vim.lsp.buf.remove_workspace_folder()<CR>', opts)
-  buf_set_keymap('n', '<leader>lwl', '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders()))<CR>', opts)
-  buf_set_keymap('n', '<leader>lD', '<cmd>lua vim.lsp.buf.type_definition()<CR>', opts)
-  buf_set_keymap('n', '<leader>lrn', '<cmd>lua vim.lsp.buf.rename()<CR>', opts)
-  buf_set_keymap('n', '<leader>lca', '<cmd>lua vim.lsp.buf.code_action()<CR>', opts)
-  buf_set_keymap('n', '<leader>lf', '<cmd>lua vim.lsp.buf.formatting()<CR>', opts)
+  map('n', 'gD', function() lsp.declaration() end, { desc = "Go to declaration"})
+  map('n', 'gd', function() lsp.definition() end, { desc = "Go to definition"})
+  map('n', 'gr', function() lsp.references() end, { desc = "List references" })
+  map('n', 'gi', function() lsp.implementation() end, { desc = "Go to implementation"})
+  map('n', 'K', function() lsp.hover() end, { desc = "Hover" })
+  map('n', '<C-k>', function() lsp.signature_help() end, { desc = "Signature help" })
+  map('n', '<localleader>wa', function() lsp.add_workspace_folder() end, { desc = "Workspace Add folder" })
+  map('n', '<localleader>wr', function() lsp.remove_workspace_folder() end, { desc = "Workspace Delete folder" })
+  map('n', '<localleader>wl', function() print(vim.inspect(lsp.list_workspace_folders())) end, { desc = "Workspace list folder" })
+  map('n', '<localleader>d', function() lsp.type_definition() end, { desc = "Type definition" })
+  map('n', '<localleader>rn', function() lsp.rename() end, { desc = "Rename" })
+  map('n', '<localleader>ca', function() lsp.code_action() end, { desc = "Code action" })
+  map('v', '<localleader>ca', function() lsp.range_code_action() end, { desc = "Code action" })
+  map('n', '<localleader>F', function() lsp.formatting() end, { desc = "formatting" })
+  map('v', '<localleader>F', function() lsp.range_formatting() end, { desc = "formatting" })
 
-  -- telescope TODO: make this safer
-  buf_set_keymap('n', '<leader>lgd', '<cmd>lua require("telescope.builtin").lsp_definitions()<cr>', opts)
-  buf_set_keymap('n', '<leader>lgt', '<cmd>lua require("telescope.builtin").lsp_type_definitions()<cr>', opts)
-  buf_set_keymap('n', '<leader>lgi', '<cmd>lua require("telescope.builtin").lsp_implementations()<cr>', opts)
-  buf_set_keymap('n', '<leader>lgr', '<cmd>lua require("telescope.builtin").lsp_references()<cr>', opts)
+  -- telescope
+  local ts = require("telescope.builtin")
+  map('n', '<localleader>ld', function() ts.lsp_definitions() end, { desc = "telescope definition" })
+  map('n', '<localleader>lt', function() ts.lsp_type_definitions() end, { desc = "telescope type definition" })
+  map('n', '<localleader>li', function() ts.lsp_implementations() end, { desc = "telescope implementation" })
+  map('n', '<localleader>lr', function() ts.lsp_references() end, { desc = "telescope references" })
+  map('n', '<localleader>ls', function() ts.lsp_document_symbols() end, { desc = "telescope document symbols" })
+  map('n', '<localleader>lS', function() ts.lsp_workspace_symbols() end, { desc = "telescope workspace symbols" })
 end
 
--- TODO: understand this
--- FIX: this keep send error when stop lsp servers
 local function lsp_highlight_document(client)
-  -- Set autocommands conditional on server_capabilities
   if client.server_capabilities.document_highlight then
     vim.cmd [[
       augroup lsp_document_highlight
