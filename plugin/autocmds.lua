c24397d574a71d2c3ac7a314cccb33f797c52143
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2022-07-10 20:27:20 +0700

    feat: silent the autocmds

--- plugin/autocmds.lua
+++ plugin/autocmds.lua
@@ -11,15 +11,16 @@ local autocmd = vim.api.nvim_create_autocmd
 --   augroup END
 -- ]])
 autocmd({ "TextYankPost" }, {
-  group = group("_general_settings"),
+  group = group "_general_settings",
   -- pattern = "*",
-  callback = function() vim.highlight.on_yank() end,
+  callback = function()
+    vim.highlight.on_yank()
+  end,
   -- command = "silent! lua vim.highlight.on_yank()", -- or use command instead of callback
-  desc = "Highlight yanked text"
+  desc = "Highlight yanked text",
 })
 
-vim.cmd(
-  [[
+vim.cmd [[
 
     " augroup _smart_relativenumber
     "   autocmd!
@@ -37,29 +38,16 @@ vim.cmd(
       autocmd FileType qf,help,man,lspinfo nnoremap <silent> <buffer> q :close<CR>
     augroup END
 
-    augroup _spell_add
-      autocmd!
+    augroup _reload_config
       autocmd BufWritePost *.utf-8.add :mkspell! ~/.config/nvim/spell/en.utf-8.add
+      autocmd BufWritePost xresources :silent !xrdb -merge ~/.config/x11/xresources
+      autocmd BufWritePost sxhkdrc :silent !pkill -USR1 -x sxhkd; notify-send sxhkd reloaded\!\!
+      autocmd BufWritePost skippy-xd.rc :silent !pkill -USR1 -x skippy-xd; notify-send skippy-xd reloaded\!\!
+      autocmd BufWritePost $XDG_CONFIG_HOME/polybar/config :silent !polybar -rq mybar && notify-send polybar reloaded\!\!
     augroup END
 
-    augroup _xresources
-      autocmd!
-      autocmd BufWritePost xresources :!xrdb -merge ~/.config/x11/xresources
-    augroup END
-
-    augroup _sxhkdrc
-      autocmd!
-      autocmd BufWritePost sxhkdrc :!pkill -USR1 -x sxhkd; notify-send sxhkd reloaded\!\!
-    augroup END
-
-    augroup _pdf_latex
-      autocmd!
-      autocmd BufWritePost *.tex :!latexmk -pdf -interaction=nonstopmode %
-    augroup END
-
-    augroup _polybar
-      autocmd!
-      autocmd BufWritePost $XDG_CONFIG_HOME/polybar/config :!polybar -rq mybar && notify-send polybar reloaded\!\!
+    augroup _compile
+      autocmd BufWritePost *.tex :silent !latexmk -pdf -interaction=nonstopmode %
     augroup END
 
     augroup _ansible
@@ -68,4 +56,3 @@ vim.cmd(
     augroup END
 
   ]]
-)
