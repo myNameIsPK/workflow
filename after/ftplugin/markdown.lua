c182790718cf207b3cfae70f01b0a41e52e00f27
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2023-03-29 21:25:48 +0700

    feat(markdonw): paste clipboard in markdonw link format

--- after/ftplugin/markdown.lua
+++ after/ftplugin/markdown.lua
@@ -2,3 +2,14 @@
 vim.opt_local.wrap = true
 vim.opt_local.linebreak = true
 vim.opt_local.breakindent = true
+vim.opt_local.conceallevel = 1
+
+local function mdlink_from_clipboard()
+  local pos = vim.api.nvim_win_get_cursor(0)
+  local clipboard = vim.fn.getreg "+"
+  vim.api.nvim_buf_set_text(0, pos[1] - 1, pos[2], pos[1] - 1, pos[2], { "[](" .. clipboard .. ")" })
+  vim.api.nvim_win_set_cursor(0, { pos[1], pos[2] + 1 })
+  vim.cmd.startinsert()
+end
+
+vim.keymap.set("n", "<leader>p", mdlink_from_clipboard, { buffer = true, desc = "Paste Markdown Link" })
