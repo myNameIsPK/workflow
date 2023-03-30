9feb32bf5bf8b6c078214c03951bba4f409ad66f
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2023-03-30 23:30:58 +0700

    refactor: plugin_installed function

--- lua/utils/plugins.lua
+++ lua/utils/plugins.lua
@@ -28,22 +28,20 @@ function M.bootstrap_lazy()
   vim.opt.rtp:prepend(lazypath)
 end
 
-local installed
 ---Check if a plugin is on the system not whether or not it is loaded
 ---@param plugin_name string
 ---@return boolean
 function M.plugin_installed(plugin_name)
-  if not installed then
+  if not my.installed_plugins then
     local dirs = fn.expand(fn.stdpath "data" .. "/site/pack/packer/start/*", true, true)
-    local opt = fn.expand(fn.stdpath "data" .. "/site/pack/packer/opt/*", true, true)
-    local lazy = fn.expand(fn.stdpath "data" .. "/lazy/*", true, true)
-    vim.list_extend(dirs, opt)
-    vim.list_extend(dirs, lazy)
-    installed = vim.tbl_map(function(path)
+    vim.list_extend(dirs, fn.expand(fn.stdpath "data" .. "/site/pack/packer/opt/*", true, true))
+    vim.list_extend(dirs, fn.expand(fn.stdpath "data" .. "/lazy/*", true, true))
+    vim.list_extend(dirs, fn.expand(vim.env.HOME .. "/.local/src/nvim-plugins/*", true, true))
+    my.installed_plugins = vim.tbl_map(function(path)
       return fn.fnamemodify(path, ":t")
     end, dirs)
   end
-  return vim.tbl_contains(installed, plugin_name)
+  return vim.tbl_contains(my.installed_plugins, plugin_name)
 end
 
 return M
