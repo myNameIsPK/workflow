6d780830d32da1b2fb4c3a54fcad8e0034ae83f5
myNameIsPk <43566401+myNameIsPK@users.noreply.github.com>
2023-06-04 22:00:04 +0700

    SYNC: 2023-06-04 22-00-04

---
+++ .local/src/my_qmk/xiudi/xd75/my_maps/config.h
@@ -0,0 +1,11 @@
+// TODO: what this?
+#pragma once
+// place overrides here
+#undef BACKLIGHT_BREATHING
+
+#define TAPPING_TERM 200
+
+#define IGNORE_MOD_TAP_INTERRUPT
+
+#define ONESHOT_TAP_TOGGLE 2  /* Tapping this number of times holds the key until tapped once again. */
+#define ONESHOT_TIMEOUT 5000  /* Time (in ms) before the one shot key is released */
