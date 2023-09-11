27a92e34cd95f6769b1973ad50c1d45765bf7c2b
kampanart <43566401+kampanart@users.noreply.github.com>
2023-09-12 04:26:48 +0700

    redo init.el

--- init.el
+++ init.el
@@ -4,43 +4,34 @@
 (add-to-list 'default-frame-alist '(alpha 100 100))
 
 ;;; General Settings
-(setq inhibit-startup-message t
-      visible-bell t)
-
-;; no gui dialog box
-(setq use-dialog-box nil)
-
-;; no graphic interfaces
 (menu-bar-mode -1)
 (scroll-bar-mode -1)
 (tool-bar-mode -1)
 (tooltip-mode -1)
-;; (set-fringe-mode 10)
 
-;; history in minibuffer
-(savehist-mode 1)
-;; revert buffers when file changed
-(global-auto-revert-mode 1)
-;; including Dired and orther buffers
+(savehist-mode 1) ; minibuffer history
+(global-auto-revert-mode 1) ; revert buffers when file change on disk
 (setq global-auto-revert-non-file-buffers t)
 
-;; mouse even in terminal
+(setq inhibit-startup-message t
+      visible-bell t
+      use-dialog-box nil)
+
 (unless (display-graphic-p)
   (xterm-mouse-mode 1))
 
-(set-face-attribute 'default nil :font "JetBrainsMono Nerd Font" :height 110)
-
-(set-default-coding-systems 'utf-8)
+(defalias 'yes-or-no-p 'y-or-n-p)
 
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; ;;; Straight.el Settings
-;; (setq package-archives '(("melpa" . "https://melpa.org/packages/")
-;;                          ("melpa-stable" . "https://stable.melpa.org/packages/")
-;;                          ("org" . "https://orgmode.org/elpa/")
-;;                          ("elpa" . "https://elpa.gnu.org/packages/")
-;;                          ("nongnu-elpa" . "https://elpa.nongnu.org/nongnu/")))
+;; prevent emacs customize `init.el' file
+(setq custom-file (expand-file-name "custom.el" user-emacs-directory))
 
+(setq auto-save-default nil ; no `#filename#'
+      make-backup-files nil ; no `filename~'
+      create-lockfiles nil) ; no `.#filename'
+
+;;; Packages
 (defvar bootstrap-version)
 (let ((bootstrap-file
        (expand-file-name "straight/repos/straight.el/bootstrap.el" user-emacs-directory))
@@ -53,134 +44,38 @@
       (goto-char (point-max))
       (eval-print-last-sexp)))
   (load bootstrap-file nil 'nomessage))
-
 (setq straight-use-package-by-default 1)
-
-;; Use straight.el for use-package expressions
 (straight-use-package 'use-package)
+; (setq use-package-always-defer 1
+;      use-package-always-ensure 1)
 
-;;; Clean emacs directory
-;; Change the user-emacs-directory to keep unwanted things out of ~/.emacs.d
-(setq user-emacs-directory (expand-file-name "~/.cache/emacs/")
-      url-history-file (expand-file-name "url/history" user-emacs-directory))
+;;; Clean Directory
 
-;; don't create lock file
-(setq create-lockfiles nil)
-
-;; no `#filename#` https://www.victorquinn.com/emacs-prevent-autosave-mess
-(setq auto-save-default nil)
-
-;; Use no-littering to automatically set common paths to the new user-emacs-directory
-(use-package no-littering)
-
-;; Keep customization settings in a temporary file (thanks Ambrevar!)
-(setq custom-file
-      (if (boundp 'server-socket-dir)
-          (expand-file-name "custom.el" server-socket-dir)
-        (expand-file-name (format "emacs-custom-%s.el" (user-uid)) temporary-file-directory)))
-(load custom-file t)
-
-;;; Color theme
-(use-package gruvbox-theme
+(use-package no-littering
   :init
-  (load-theme 'gruvbox-light-soft t))
+  (setq no-littering-etc-directory
+          (expand-file-name ".local/etc" user-emacs-directory)
+        no-littering-var-directory
+          (expand-file-name ".local/var" user-emacs-directory)))
 
-;;; Evil
+;;; Undo Tree
 (use-package undo-tree
-  :init
+  :config
   (global-undo-tree-mode 1))
 
-;; for `g;` ahd `g,` motions
-(use-package goto-chg)
-
-(global-set-key (kbd "C-M-u") 'universal-argument)
-
+;;; Evil
 (use-package evil
   :init
-  (setq evil-want-integration t)
-  (setq evil-want-keybinding nil)
-  (setq evil-want-C-u-scroll t)
-  (setq evil-respect-visual-line-mode t)
-  (setq evil-undo-system 'undo-tree)
+  (setq evil-want-keybinding nil
+	evil-want-C-u-scroll t)
   :config
   (evil-mode 1)
-  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
-  (define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
-
+  (evil-set-undo-system 'undo-tree)
   (define-key evil-normal-state-map (kbd "C-n") 'evil-next-line)
-  (define-key evil-normal-state-map (kbd "C-p") 'evil-previous-line)
+  (define-key evil-normal-state-map (kbd "C-p") 'evil-previous-line))
 
-  (evil-set-initial-state 'messages-buffer-mode 'normal)
-  (evil-set-initial-state 'dashboard-mode 'normal))
+;;; Git
+(use-package magit)
 
-(use-package evil-collection
-  :after evil
-  :config
-  (evil-collection-init))
-
-;;; Which key
-(use-package which-key
-  :init (which-key-mode)
-  :config
-  (setq which-key-idle-delay 0.3))
-
-;;;; Completions
-;;; Minibuffer Completions
-(use-package vertico
-  :init
-  (vertico-mode)
-  ;; Show more candidates
-  ;; (setq vertico-count 20)
-  (setq vertico-resize t)
-  (setq vertico-cycle t))
-
-(use-package orderless
-  :init
-  (setq completion-styles '(orderless basic)
-        completion-category-defaults nil
-        completion-category-overrides '((file (styles partial-completion)))))
-
-(use-package marginalia
-  ;; Either bind `marginalia-cycle` globally or only in the minibuffer
-  :bind (("M-A" . marginalia-cycle)
-         :map minibuffer-local-map
-         ("M-A" . marginalia-cycle))
-
-  ;; The :init configuration is always executed (Not lazy!)
-  :init
-
-  ;; Must be in the :init section of use-package such that the mode gets
-  ;; enabled right away. Note that this forces loading the package.
-  (marginalia-mode))
-
-(use-package consult)
-(use-package embark
-  :bind (("C-S-a" . embark-act)
-         :map minibuffer-local-map
-         ("C-S-a" . embark-act)))
-
-
-;;; Completion in region
-(use-package corfu
-  :custom
-  (corfu-cycle t)                ;; Enable cycling for `corfu-next/previous'
-  (corfu-auto t)                 ;; Enable auto completion
-  (corfu-separator ?\s)          ;; Orderless field separator
-  ;; (corfu-quit-at-boundary nil)   ;; Never quit at completion boundary
-  ;; (corfu-quit-no-match nil)      ;; Never quit, even if there is no match
-  ;; (corfu-preview-current nil)    ;; Disable current candidate preview
-  ;; (corfu-preselect-first nil)    ;; Disable candidate preselection
-  ;; (corfu-on-exact-match nil)     ;; Configure handling of exact matches
-  ;; (corfu-echo-documentation nil) ;; Disable documentation in the echo area
-  ;; (corfu-scroll-margin 5)        ;; Use scroll margin
-
-  ;; Enable Corfu only for certain modes.
-  ;; :hook ((prog-mode . corfu-mode)
-  ;;        (shell-mode . corfu-mode)
-  ;;        (eshell-mode . corfu-mode))
-
-  ;; Recommended: Enable Corfu globally.
-  ;; This is recommended since Dabbrev can be used globally (M-/).
-  ;; See also `corfu-excluded-modes'.
-  :init
-  (global-corfu-mode))
+;;; Completion
+(fido-vertical-mode 1) ; fuzzy find
