d9b54ce3ac224b209fd0e9a591e9aab77bdff94c
kampanart <43566401+kampanart@users.noreply.github.com>
2023-10-05 16:30:04 +0700

    SYNC: 2023-10-05 16-30-04

--- scripts/pacman-list.sh
+++ scripts/pacman-list.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-pacman -Qem > /home/pk/pacman-aur.txt 2>/dev/null
-pacman -Qen > /home/pk/pacman.txt 2>/dev/null
+pacman -Qem > /home/pk/notes/data/pacman-aur.txt 2>/dev/null
+pacman -Qen > /home/pk/notes/data/pacman.txt 2>/dev/null
