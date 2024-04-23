4728c46a141342597325fc6038cbce64d50450d7
kampanart <43566401+kampanart@users.noreply.github.com>
2024-04-23 08:30:03 +0700

    SYNC: 2024-04-23 08-30-03

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,16 +1,16 @@
 local wezterm = require("wezterm")
 local action = wezterm.action
+local config = {}
 
-config = {}
-config.color_scheme = "Gruvbox light, soft (base16)"
+-- config.color_scheme = "Gruvbox light, soft (base16)"
 config.use_fancy_tab_bar = false
 config.hide_tab_bar_if_only_one_tab = true
 config.font_size = 11
-config.disable_default_key_bindings = true
+-- config.enable_scroll_bar = true
 config.window_padding = {
 	left = 2,
-	right = 2,
-	top = 10,
+	right = 2, -- also width of scroll bar
+	top = 0,
 	bottom = 0,
 }
 config.cursor_blink_rate = 500
@@ -19,10 +19,11 @@ config.cursor_blink_ease_out = "Constant"
 -- disable ligature
 config.harfbuzz_features = { "calt=0", "clig=0", "liga=0" }
 
+config.disable_default_key_bindings = true
 local default_mods = "CTRL|SHIFT|ALT"
 config.keys = {
-	{ key = "L", mods = default_mods, action = action.ShowDebugOverlay },
-	{ key = "X", mods = default_mods, action = action.ActivateCopyMode },
+	{ key = "l", mods = default_mods, action = action.ShowDebugOverlay },
+	{ key = "Escape", mods = default_mods, action = action.ActivateCopyMode },
 }
 
 return config
