cd8319238c8cbc6bfc007bade01dbe941cca8e09
kampanart <43566401+kampanart@users.noreply.github.com>
2024-04-23 07:30:04 +0700

    SYNC: 2024-04-23 07-30-04

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -1,5 +1,17 @@
-return {
+local wezterm = require("wezterm")
+local action = wezterm.action
+
+config = {
 	color_scheme = "Gruvbox light, soft (base16)",
 	use_fancy_tab_bar = false,
 	hide_tab_bar_if_only_one_tab = true,
+	font_size = 11,
+	disable_default_key_bindings = true,
 }
+
+local default_mods = "CTRL|SHIFT|ALT"
+config.keys = {
+	{ key = "L", mods = default_mods, action = action.ShowDebugOverlay },
+}
+
+return config
